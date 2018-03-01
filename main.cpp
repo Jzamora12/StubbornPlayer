4d942cbea08a36539d08ff734cd3a8599178ac53
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-03-01 10:43:26 -0800

    declared variables and ask user for how many game they want to play.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int games;
+  int wins=0;
+  cout<< "How many games do you want to play?\n";
+  cin>> games;
+  for ( int i=0; i<games; i++) 
+  {
+    game.newGame(); 
+    game.guessDoorC();
+     if ( game.isWinner() == true )
+        {
+          wins++;
+        }   
+  }
+  cout<<"You won "<<wins;
+  cout<<" games.\n";
   return 0;
 }
